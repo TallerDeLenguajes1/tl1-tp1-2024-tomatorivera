2be2fd5dc6e04312c687e202e7832816a42b7313
tomatorivera <tomatorivera@gmail.com>
2024-03-26 16:45:06 -0300

    Punto 2 f)

--- tp1_1.c
+++ tp1_1.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     printf("Hola mundo");
+
+    int var = 10;
+    int *puntero = &var;
+
+    printf("\n\nContenido del puntero: %d", *puntero);
+    printf("\nDireccion de memoria de 'var': %p", puntero);
+    printf("\nDireccion de memoria del puntero: %p", &puntero);
+    printf("\nTamanio de la variable: %d", sizeof(var));
 
     return 0;
 }
